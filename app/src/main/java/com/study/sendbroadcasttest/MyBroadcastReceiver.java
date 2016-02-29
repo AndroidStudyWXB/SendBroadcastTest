aec5252c44d28b08e0dc58a48e642971e0c2cf24
WXB506 <1922686887@qq.com>
2016-02-29 21:32:13 +0800

    initial version, standard broadcast

---
+++ app/src/main/java/com/study/sendbroadcasttest/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.study.sendbroadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by WXB506 on 2016/2/29.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_LONG).show();
+    }
+
+}
